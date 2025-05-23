4d02a9aa0b4d13f6da3448a350f82411674f8417
eliyahuA <eliyahu.arlev@gmail.com>
2025-05-23 21:02:09 +0300

    implement tests for the factory

--- tests/unit/endpoints/factories/test_yaml_factory.py
+++ tests/unit/endpoints/factories/test_yaml_factory.py
@@ -1,14 +1,53 @@
+import pytest
+import yaml
 from tasepy.endpoints.factories import YAMLFactory
+from tasepy.requests_.urls import BaseModelGeneric
+from typing import Generic, Type, Dict, Any, Generator
+from pydantic import BaseModel
+from pathlib import Path
+
+
+class DummyModel(BaseModel, Generic[BaseModelGeneric]):
+    field: str
+
+
+@pytest.fixture
+def pydantic_model() -> Type[BaseModelGeneric]:
+    return DummyModel
+
+
+@pytest.fixture
+def dummy_dict() -> Dict[str, Any]:
+    return {'field': 'value'}
+
+
+@pytest.fixture
+def dummy_yaml_file(tmp_path, dummy_dict) -> Generator[Path]:
+    yaml_file_path = tmp_path / 'dummy.yaml'
+    with open(yaml_file_path, 'w') as f:
+        yaml.dump(dummy_dict, f)
+    yield yaml_file_path
+
+
+@pytest.fixture
+def dummy_yaml_file_str_path(dummy_yaml_file):
+    return str(dummy_yaml_file)
 
 
 class TestYAMLFactory:
 
-    def test_get_endpoints(self):
-        import tasepy
-        from pathlib import Path
-        from tasepy.requests_.urls import Endpoints
-        package_dir = Path(tasepy.__file__).parent
-        yaml_path = package_dir / "endpoints" / "endpoints.yaml"
-        yaml_factory = YAMLFactory(yaml_path, Endpoints)
-        endpoints = yaml_factory.get_endpoints()
-        assert True
+    @pytest.mark.parametrize(
+        "dummy_dict,pydantic_model",
+        [
+            (dummy_dict, pydantic_model),
+            (dummy_yaml_file, pydantic_model),
+            (dummy_yaml_file_str_path, pydantic_model)
+        ],
+        indirect=True
+    )
+    def test_get_endpoints(self, dummy_dict, pydantic_model):
+        yaml_factory = YAMLFactory(dummy_dict, pydantic_model)
+        loaded_model = yaml_factory.get_endpoints()
+        assert isinstance(loaded_model, DummyModel)
+        assert 'field' in loaded_model.model_fields.keys()
+        assert loaded_model.field == 'value'
