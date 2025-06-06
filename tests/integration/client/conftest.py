830111bd307c15441cf7a873b23e4d7544d0aa1c
eliyahuA <eliyahu.arlev@gmail.com>
2025-06-06 16:54:52 +0300

    restructure tests

---
+++ tests/integration/client/conftest.py
@@ -0,0 +1,29 @@
+import tasepy
+
+from tasepy.settings import SettingsBuilder
+from tasepy.endpoints.factories.yaml_factory import YAMLFactory
+from tasepy.clients.tailored import Client
+from pathlib import Path
+from importlib import resources
+from pytest import fixture
+from tasepy.requests_.urls import Endpoints
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+@fixture()
+def settings():
+    return (SettingsBuilder()
+            .with_apikey(environment="API_KEY")
+            .build())
+
+
+@fixture()
+def url_model_factory():
+    return YAMLFactory(Path(str(resources.files(tasepy))) / 'endpoints' / 'endpoints.yaml', Endpoints)
+
+
+@fixture()
+def client(settings, url_model_factory):
+    return Client(settings, url_model_factory)
