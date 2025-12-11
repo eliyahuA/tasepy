64eb39e2932ee0af6108d8d545b3e8ae1adeed42
Dmitry Messerman <dmitry.messerman@gmail.com>
2025-12-11 17:10:57 +0200

    Fixed typo

--- tests/examples/test_tasepy_main_indeces.py
+++ tests/examples/test_tasepy_main_indeces.py
@@ -1,7 +1,7 @@
 import sys
 from tasepy.examples import tasepy_main_indices
 
-def test_tase_api_site(monkeypatch):
+def test_tasepy_main_indices(monkeypatch):
     with monkeypatch.context() as m:
         m.setattr(sys, 'argv', ['tasepy_main_indices'])
         assert tasepy_main_indices.main() == 0
